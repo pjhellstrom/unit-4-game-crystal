6b40b33cf88ad1b21eed108d432e4ce824f50c1e
Jonas H <pjh.hellstrom@gmail.com>
2019-07-09 22:50:47 -0400

    fully built out with added game over screen and replay option

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,36 +1,48 @@
 // Set variables --------------------------------------------------------
-var goalValue = 0;
-var goalValueMax = 200;
+const goalValueMax = 10;
+const crystalValueMax = 20;
 var crystalValues = [0, 0, 0, 0];
-var crystalValueMax = 20;
+var goalValue = 0;
 var guessValue = 0;
 var pointsAdded = 0;
 var winCount = 0;
 var loseCount = 0;
 
-var a = 3;
-var b = "3";
-var c = a + b;
-console.log(c);
+// Initialize game -------------------------------------------------------
+$(document).ready(function() {
+    // From start screen
+    $("#startButton").click(hideStartScreen);
+    $("#startButton").click(setValues);
+    $("#startButton").click(updateStats);
+    $("#startButton").click(showGameScreen);
+    $("#startButton").click(getClick);
+    
+    // From game over screen
+    $("#restartButton").click(hideEndScreen);
+    $("#restartButton").click(setValues);
+    $("#restartButton").click(updateStats);
+    $("#restartButton").click(showGameScreen);
+    $("#restartButton").click(getClick);
+});
 
-// Randomize goal value --------------------------------------------------
-goalValue = Math.floor(Math.random() * goalValueMax);
+// Functions -------------------------------------------------------------
 
-// Randomize crystal values ----------------------------------------------
-for (i = 0; i < crystalValues.length; i++) {
-  crystalValues[i] = Math.floor(Math.random() * crystalValueMax);
-}// end for-loop
+function setValues() {
+    // Randomize goal value ----------------------------------------------
+    goalValue = Math.floor(Math.random() * goalValueMax);
 
-// Functions ----------------------------------------------------------
+    // Randomize crystal values ------------------------------------------
+    for (i = 0; i < crystalValues.length; i++) {
+    crystalValues[i] = Math.floor(Math.random() * crystalValueMax);
+    }// end for-loop
+}//end setValues()
 
-// function getClick() {
-    $(document).ready(function() {
+function getClick() {
     $("#crystal0").click( function() {addPoints( $("#crystal0").attr("value") )});
     $("#crystal1").click( function() {addPoints( $("#crystal1").attr("value") )});
     $("#crystal2").click( function() {addPoints( $("#crystal2").attr("value") )});
     $("#crystal3").click( function() {addPoints( $("#crystal3").attr("value") )});
-    })
-// }// end getClick()
+}// end getClick()
     
 function addPoints(a) {
     pointsAdded = crystalValues[a];
@@ -40,34 +52,103 @@ function addPoints(a) {
 }// end addPoints()
 
 function updateStats() {
-    console.log("LOOK AT THIS too: " + guessValue);
-    $("#goalValue").text(goalValue)
-    $("#guessValue").text(guessValue)
-    $("#winCount").text(winCount)
-    $("#loseCount").text(loseCount) 
+        $("#goalValue").text(goalValue)
+        $("#guessValue").text(guessValue)
+        $("#winCount").text(winCount)
+        $("#loseCount").text(loseCount) 
 }// end updateStats()
+
+function resetStats() {
+    crystalValues = [0, 0, 0, 0];
+    goalValue = 0;
+    guessValue = 0;
+    pointsAdded = 0;
+}// end reset()
+
+function resetWinLose() {
+    winCount = 0;
+    loseCount = 0;        
+}
 
 // Decision function -----------------------------------------------------
 function decision() {
 
     // Case1 : if guess is under goal - allow more guesses
     if (guessValue < goalValue) {
-        getClick();
+        return;
     }// end if
 
-    // Case2 : if guess is under goal - allow more guesses
+    // Case2 : if guess is equal to goal - add to win count, setup new session
     else if (guessValue == goalValue) {
         // Alert winner
-        alert("Winner!")
+        alert("That's the number!");
+        winCount++;
+            // Check for game over
+            if (winCount > 2) {
+                resetStats();
+                resetWinLose();
+                hideGameScreen();
+                    // Setup end screen
+                    $("#endScreen").removeClass("bg-warning")
+                    $("#endScreen").addClass("bg-success")
+                    $("#endHeader").text("You did it!")
+                showEndScreen();
+            }
+            else {
+            updateStats();
+            resetStats();
+            setValues();
+            }
     }// end else if
 
-    // Case3 : if guess is under goal - allow more guesses
-    else if (guessValue > goalValue) {
+    // Case3 : else guess is over goal - add to lose count, setup new session
+    else {
         // Alert game over
-        alert("Game over!")
+        alert("Too high!");
+        loseCount++;
+            // Check for game over
+            if (loseCount > 2) {
+                resetStats();   
+                resetWinLose();             
+                hideGameScreen();
+                    // Setup end screen
+                    $("#endScreen").removeClass("bg-warning")
+                    $("#endScreen").addClass("bg-danger")
+                    $("#endHeader").text("Sorry, try again!")
+                showEndScreen();
+            }
+            else {
+            updateStats();
+            resetStats();
+            setValues();
+            }
     }// end else
 
 }// end decision()
+
+
+// Hiding, showing elements ------------------------------------------
+
+function hideStartScreen() {
+    $("#startScreen").hide();
+}
+
+function showGameScreen() {
+    $("#gameScreen").show();
+}
+
+function hideGameScreen() {
+    $("#gameScreen").hide();
+}
+
+function showEndScreen() {
+    $("#endScreen").show();
+}
+
+function hideEndScreen() {
+    $("#endScreen").hide();
+}
+
 
 // Console log ----------------------------------------------------------
 console.log("random number array: " + crystalValues)
